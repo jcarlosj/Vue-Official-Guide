e2ec992340a5254f9d0eac764dc5473a63851ecc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-04 18:16:27 -0500

    [+] Introduccion: Composicion de Componentes

--- js/App.js
+++ js/App.js
@@ -1,20 +1,27 @@
-/** Manejando Interacciones del Usuario */
+/** Composición con Componentes */
+
+// Define un nuevo componente llamado todo-item
+Vue .component( 'todo-item', {
+    template: '<li>Esta es una tarea.</li>'
+});
 
 var app = new Vue({             // Instancia de Vue
-    el: '#app',                 // Elemento al que hace referencia la Instancia
-    data: {                     // Definición de propiedades
-        message: 'Hola Vue.js!'
-    },
-    methods: {                  // Definicion de Metodos 
-        reverseMessage: function () {
-            this .message = this .message .split('') .reverse() .join('')
-        }
-    }
+    el: '#app'                  // Elemento al que hace referencia la Instancia
+});
+
+// Define un nuevo componente llamado grocery-item
+Vue .component( 'grocery-item', {
+    props: [ 'article' ],          // propiedad 'article' atributo personalizado (usado para hacer el bind en la vista)
+    template: '<li>{{ article.text }}</li>'
 });
 
 var app2 = new Vue({
     el: '#app-2',
     data: {
-        message: 'Hola Vue.js!'
+        groceryList: [
+            { id: 0, text: 'Vegetales' },
+            { id: 1, text: 'Queso' },
+            { id: 2, text: 'Cualquier otra cosa que se supone que los humanos coman' }
+        ]
     }
 });
