a20c6e4091295b540217109a45357047b661d523
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-04 19:29:10 -0500

    [+] La instancia Vue: Interpolaciones Atributos

--- js/App.js
+++ js/App.js
@@ -3,6 +3,14 @@
 var app = new Vue({             // Instancia de Vue
     el: '#app',                 // Elemento al que hace referencia la Instancia
     data: {                     // Definición de propiedades
-        message: `<span style="color: blue">Hola a todos, bienvenidos!</span>`
+        dynamicId: 'title',
+        isButtonDisabled: false
+    },
+    methods: {
+        removeIdSelector: function () {
+            this .dynamicId = '';
+            this .isButtonDisabled = true;
+            console .log( 'Elimina ID de estilo del texto y deshabilita el botón' );
+        }
     }
 });
