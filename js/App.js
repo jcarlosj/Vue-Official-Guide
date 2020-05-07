b1d8589e81c6f6750c47beda5701b5d18d0b858e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-07 11:38:33 -0500

    [+] Propiedades Computadas: Caching computado Vs Metodos

--- js/App.js
+++ js/App.js
@@ -1,31 +1,18 @@
-/** Directivas */
+/** Propiedades Computadas */
 
 var app = new Vue({             // Instancia de Vue
     el: '#app',                 // Elemento al que hace referencia la Instancia
+    data: {
+        message: ''
+    },
     methods: {
         doSomething: function ( event ) {
-            event .preventDefault();
-
-            console .log( event .target .name .value );
-            alert( `Hola ${ event .target .name .value }! \nBienvenido a Vue.js` );
+            this .message = event .target .name .value;
         }
-    }
-});
-
-var app2 = new Vue({             // Instancia de Vue
-    el: '#app-2',                // Elemento al que hace referencia la Instancia
-    methods: {
-        doSomething: function ( event ) {
-
-            if( event .target .name .value .length < 'vue' .length ) {
-                alert( `Vaya! tu nombre es mas corto que 'Vue'` );
-            }
-            else if( event .target .name .value .length === 'vue' .length ) {
-                alert( `La longitud de tu nombre es igual a la de 'Vue'` );
-            }
-            else {
-                alert( `Hola ${ event .target .name .value }! \nBienvenido a Vue.js` );
-            }
+    },
+    computed: {
+        reversedMessase: function ( event ) {
+            return this .message .split('') .reverse() .join('');
         }
     }
 });
