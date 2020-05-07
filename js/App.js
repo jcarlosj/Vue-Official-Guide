6d94f52f955e61d1f69a1ac09b2240e48ab29b41
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-07 12:05:18 -0500

    [+] Propiedades Computadas Vs Observador de propiedades

--- js/App.js
+++ js/App.js
@@ -1,13 +1,31 @@
 /** Propiedades Computadas */
 
-var app = new Vue({             // Instancia de Vue
-    el: '#app',                 // Elemento al que hace referencia la Instancia
+var app = new Vue({
+    el: '#app',
     data: {
-        message: ''
+        firstName: 'Ana Maria',
+        lastName: 'Castro Chavez'
     },
     computed: {
-        reversedMessase: function ( event ) {
-            return this .message .split('') .reverse() .join('');
+        fullName: function () {
+            return `${ this .firstName } ${ this .lastName }`
+        }
+    }
+});
+
+var app2 = new Vue({             // Instancia de Vue
+    el: '#app-2',                // Elemento al que hace referencia la Instancia
+    data: {
+        firstName: 'Luisa Maria',
+        lastName: 'Bazalar Salazar',
+        fullName: 'Luisa Maria Bazalar Salazar'
+    },
+    watch: {                    // Observa por los cambios de cada  uno de las propiedades incluidas en el
+        firstName: function ( value ) {
+            this .fullName = `${ value } ${ this .lastName }`;
+        },
+        lastName: function ( value ) {
+            this .fullName = `${ this .firstName } ${ value }`;
         }
     }
 });
