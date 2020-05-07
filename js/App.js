ebc85c7076f938d3b125ae132296211b9df7ffc8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-07 10:43:30 -0500

    [+] Directivas: v-if

--- js/App.js
+++ js/App.js
@@ -1,28 +1,8 @@
-/** Interpolaciones */
+/** Directivas */
 
 var app = new Vue({             // Instancia de Vue
     el: '#app',                 // Elemento al que hace referencia la Instancia
     data: {                     // Definición de propiedades
-        applyStyle: false
-    },
-    methods: {                  // Definicion de Metodos
-        removeIdSelector: function () {
-            this .applyStyle = ! this .applyStyle;
-            console .log( `${ this .applyStyle ? 'Pone ' : 'Quita' } el estilo` );
-        }
-    }
-});
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'Hola Vue.js!'
-    }
-});
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        message: 'Hola Vue.js!'
+        seen: true
     }
 });
