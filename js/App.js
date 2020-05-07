d6bdd45e4651b88e3833463d3c7ed68ea5e506ec
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-07 11:20:23 -0500

    [+] Directiva: Modificadores

--- js/App.js
+++ js/App.js
@@ -2,13 +2,30 @@
 
 var app = new Vue({             // Instancia de Vue
     el: '#app',                 // Elemento al que hace referencia la Instancia
-    data: {                     // Definición de propiedades
-        url: 'https://es.vuejs.org/'
-    },
     methods: {
-        doSomething: function () {
-            console .log( 'Hola Vuejs!' );
-            alert( 'Hola Vuejs!' );
+        doSomething: function ( event ) {
+            event .preventDefault();
+
+            console .log( event .target .name .value );
+            alert( `Hola ${ event .target .name .value }! \nBienvenido a Vue.js` );
+        }
+    }
+});
+
+var app2 = new Vue({             // Instancia de Vue
+    el: '#app-2',                // Elemento al que hace referencia la Instancia
+    methods: {
+        doSomething: function ( event ) {
+
+            if( event .target .name .value .length < 'vue' .length ) {
+                alert( `Vaya! tu nombre es mas corto que 'Vue'` );
+            }
+            else if( event .target .name .value .length === 'vue' .length ) {
+                alert( `La longitud de tu nombre es igual a la de 'Vue'` );
+            }
+            else {
+                alert( `Hola ${ event .target .name .value }! \nBienvenido a Vue.js` );
+            }
         }
     }
 });
