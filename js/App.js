bfce37cd1a1740cfcf031f9c0f0c9220a960dd3b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-07 10:52:22 -0500

    [+] Directivas: Argumentos

--- js/App.js
+++ js/App.js
@@ -3,6 +3,12 @@
 var app = new Vue({             // Instancia de Vue
     el: '#app',                 // Elemento al que hace referencia la Instancia
     data: {                     // Definición de propiedades
-        seen: true
+        url: 'https://es.vuejs.org/'
+    },
+    methods: {
+        doSomething: function () {
+            console .log( 'Hola Vuejs!' );
+            alert( 'Hola Vuejs!' );
+        }
     }
 });
